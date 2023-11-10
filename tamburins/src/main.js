b980cd3c6d2b7aa7a46b5672b48cd68721462286
so-dami <so94dami@gmail.com>
2023-11-10 22:01:03 +0900

    ㄴ

--- tamburins/src/main.js
+++ tamburins/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import router from './router.js';
 import store from './store.js';
 let app = createApp(App);
